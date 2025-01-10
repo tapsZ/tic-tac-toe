78a9ddef2f2cd73f4e8f35c632d4d940a63b3d82
Tapiwa Zireva <tapiwazireva@gmail.com>
2025-01-10 09:55:30 +0200

    only single player working at the moment

--- src/components/game_board/GameBoard.jsx
+++ src/components/game_board/GameBoard.jsx
@@ -1,17 +1,35 @@
+import { useState } from "react";
+
 export default function GameBoard() {
   const initialBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null],
   ];
+  const [gameBoard, setGameBoard] = useState(initialBoard);
+
+  const handlePlayClick = (rowIndex, columnIndex) => {
+      setGameBoard((updatedGameBoard) => {
+          let newGameBoard = [
+              ...updatedGameBoard.map((innerArray) => [...innerArray]),
+          ];
+          newGameBoard[rowIndex] [columnIndex] = "X";
+      return newGameBoard;
+    });
+  };
+
   return (
     <ol id="game-board">
-      {initialBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, playerSymbolIndex) => (
               <li key={playerSymbolIndex}>
-                <button>{playerSymbol}</button>
+                <button
+                  onClick={() => handlePlayClick(rowIndex, playerSymbolIndex)}
+                >
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
